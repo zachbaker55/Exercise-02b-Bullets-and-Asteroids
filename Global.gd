d01289deb373920b33d7663dad20f6dd9e14b276
zachbaker55 <thezachbaker55@gmail.com>
2022-09-15 20:37:50 -0400

    finished exercise

--- Global.gd
+++ Global.gd
@@ -1,5 +1,25 @@
 extends Node
 
+var hue = 0
+var color = Color.from_hsv(hue, 0.5, 0.79, 0.8) 
+
+
+
 func _unhandled_input(event):
 	if event.is_action_pressed("menu"):
 		get_tree().quit()
+
+func _ready():
+	randomize()
+
+func _process(delta):
+	changeBGColor(delta);
+	
+	
+	
+func changeBGColor(delta):
+	if hue >= 1:
+		hue = 0;
+	color = Color.from_hsv(hue, 0.5, 0.9, 1)
+	VisualServer.set_default_clear_color(color)
+	hue += 0.01 * delta;
--- Player/Player.gd
+++ Player/Player.gd
@@ -3,26 +3,40 @@ extends KinematicBody2D
 var velocity = Vector2.ZERO
 
 var rotation_speed = 5.0
-var speed = 0.1
-var max_speed = 10
+var speed = 5.0
+var max_speed = 400.0
 
-
+onready var Bullet = load("res://Player/Bullet.tscn")
+var nose = Vector2(0,-60)
 
 func _ready():
 	pass
 
 func _physics_process(_delta):
-	position = position + velocity
+	velocity += get_input() * speed
 	velocity = velocity.normalized() * clamp(velocity.length(), 0, max_speed)
+	velocity = move_and_slide(velocity, Vector2.ZERO)
+	position.x = wrapf(position.x, 0, 1024)
+	position.y = wrapf(position.y, 0, 600)
 	
+	if Input.is_action_just_pressed("shoot"):
+		var Effects = get_node("/root/Game/Effects")
+		if  Effects != null:
+			var bullet = Bullet.instance()
+			bullet.global_position = global_position + nose.rotated(rotation)
+			bullet.rotation = rotation
+			Effects.add_child(bullet)
+	
+func get_input():
+	var to_return = Vector2.ZERO;
 	$Exhaust.hide()
 	if Input.is_action_pressed("forward"):
-		velocity = velocity + Vector2(0,-speed).rotated(rotation)
+		to_return.y -= 1
 		$Exhaust.show()
 	if Input.is_action_pressed("left"):
 		rotation_degrees = rotation_degrees - rotation_speed
 	if Input.is_action_pressed("right"):
 		rotation_degrees = rotation_degrees + rotation_speed
-		
-	position.x = wrapf(position.x, 0, 1024)
-	position.y = wrapf(position.y, 0, 600)
+	return to_return.rotated(rotation)
+	
+
